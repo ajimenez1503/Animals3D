e542f899d34f9853ba0cbc8006eb27855cbf2161
Antonio Jimenez <5960625+softwarejimenez@users.noreply.github.com>
2020-04-05 10:41:12 +0200

    Add new 3D activity #1

---
+++ app/src/main/java/com/example/animals/Activity3D.java
@@ -0,0 +1,14 @@
+package com.example.animals;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity3D extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity3_d);
+    }
+}
--- app/src/main/java/com/example/animals/MainActivity.java
+++ app/src/main/java/com/example/animals/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.animals;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the user taps the 3D button
+     */
+    public void open3DActivity(View view) {
+        Intent intent = new Intent(this, Activity3D.class);
+        startActivity(intent);
     }
 }
