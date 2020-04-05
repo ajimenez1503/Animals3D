a47a1b87827efa550d68ecf190a5338d392e53cc
Antonio Jimenez <softwarejimenezz@gmail.com>
2020-04-05 17:38:28 +0200

    add sounds animals #6

--- app/src/main/java/com/example/animals/ActivitySounds.java
+++ app/src/main/java/com/example/animals/ActivitySounds.java
@@ -1,5 +1,6 @@
 package com.example.animals;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,8 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivitySounds extends AppCompatActivity {
 
-    private static final String TAG = Activity3D.class.getSimpleName();
-
+    private static final String TAG = ActivitySounds.class.getSimpleName();
+    MediaPlayer mp = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +18,34 @@ public class ActivitySounds extends AppCompatActivity {
         setContentView(R.layout.activity_sounds);
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "Stop sounds");
+        if (mp != null) {
+            mp.stop();
+            mp.release();
+        }
+    }
+
     /**
      * Called when the user taps the Tiger button
      */
     public void playTigerSounds(View view) {
         Log.i(TAG, "Play Tiger sounds");
-
+        try {
+            if (mp == null) {
+                mp = MediaPlayer.create(this, R.raw.tiger);
+            } else if (mp.isPlaying()) {
+                mp.stop();
+                mp.release();
+                mp = MediaPlayer.create(this, R.raw.tiger);
+            }
+            mp.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "Exception: " + e.toString());
+        }
     }
 
     /**
@@ -30,6 +53,19 @@ public class ActivitySounds extends AppCompatActivity {
      */
     public void playBearSounds(View view) {
         Log.i(TAG, "Play Bear sounds");
+        try {
+            if (mp == null) {
+                mp = MediaPlayer.create(this, R.raw.bear);
+            } else if (mp.isPlaying()) {
+                mp.stop();
+                mp.release();
+                mp = MediaPlayer.create(this, R.raw.bear);
+            }
+            mp.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "Exception: " + e.toString());
+        }
 
     }
 }
