2a1237ad56969a12af7e4cd1e1de15932fe22dc5
Antonio Jimenez <softwarejimenezz@gmail.com>
2020-04-05 10:50:59 +0200

    OnClick button #1

--- app/src/main/java/com/example/animals/Activity3D.java
+++ app/src/main/java/com/example/animals/Activity3D.java
@@ -1,14 +1,32 @@
 package com.example.animals;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity3D extends AppCompatActivity {
 
+    private static final String TAG = Activity3D.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity3_d);
+    }
+
+    /**
+     * Called when the user taps the Tiger button
+     */
+    public void show3DTiger(View view) {
+        Log.i(TAG, "Open 3D Tiger");
+    }
+
+    /**
+     * Called when the user taps the Bear button
+     */
+    public void show3DBear(View view) {
+        Log.i(TAG, "Open 3D Bear");
     }
 }
